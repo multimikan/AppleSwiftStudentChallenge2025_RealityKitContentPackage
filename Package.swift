c275469d6364c6439200af5027cd14d366af0c64
multimikan <koki20041008@gmail.com>
2026-02-24 22:32:22 +0900

    ターゲットにmacosを追加

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
     platforms: [
         .iOS(.v18),
+        .macOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
