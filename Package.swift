e3c25fe40f8ee636c659160a0ce5fb6a22a81d0c
multimikan <koki20041008@gmail.com>
2026-02-24 22:12:17 +0900

    更新

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "RealityKitContent",
+    name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
     platforms: [
         .iOS(.v17),
     ],
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RealityKitContent",
-            targets: ["RealityKitContent"]),
+            targets: ["AppleSwiftStudentChallenge2025_RealityKitContentPackage"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RealityKitContent",
+            name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
             dependencies: [],
             resources: [
-                    .process("RealityKitContent.rkassets")
+                    .process("AppleSwiftStudentChallenge2025_RealityKitContentPackage.rkassets")
                 ]),
     ]
 )
