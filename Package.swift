a254c42ce9b8dcf04c62a1a33beb15cd29032787
multimikan <koki20041008@gmail.com>
2026-02-25 00:40:54 +0900

    ターゲットを微調整

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0.2
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
     platforms: [
-        .iOS(.v18),
-        .macOS(.v14)
+        .iOS("18.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
