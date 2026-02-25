94e462624133028402c46cada161f0f62c249e15
multimikan <koki20041008@gmail.com>
2026-02-25 11:54:24 +0900

    ディレクトリ関係を改善

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
     platforms: [
-        .iOS("18.0")
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -14,18 +14,14 @@ let package = Package(
             name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
             targets: ["AppleSwiftStudentChallenge2025_RealityKitContentPackage"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppleSwiftStudentChallenge2025_RealityKitContentPackage",
-            dependencies: [],
             resources: [
                     .process("AppleSwiftStudentChallenge2025_RealityKitContentPackage.rkassets")
-                ]),
+                ]
+        ),
     ]
 )
